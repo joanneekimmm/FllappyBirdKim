825e398e0442e41c07887854f92c391a353ae529
jojo <joannek715967@gmail.com>
2022-12-19 12:51:43 -0800

    Almost done with Flappy Bird
    Created a column script and its rigidbody2d, randomizing the columns, rearranged the camera to fit the view. Have to change the flapping of the bird as well as the fonts.

--- FlappyBirdFinalKim/Assets/Scripts/RepeatingBackground.cs
+++ FlappyBirdFinalKim/Assets/Scripts/RepeatingBackground.cs
@@ -16,6 +16,15 @@ public class RepeatingBackground : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < -groundHorizontalLength)
+        {
+            RepositionBackground();
+        }
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffset = new Vector2(groundHorizontalLength * 2f, 0);
+        transform.position = (Vector2)transform.position + groundOffset;
     }
 }
